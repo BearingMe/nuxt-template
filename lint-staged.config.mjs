403cfe1643a9d4b8859644037d7b4863dd4fbfff
BearingMe <gomes.bruno201@gmail.com>
2024-08-23 10:47:38 -0300

    chore: setup lint-staged

---
+++ lint-staged.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  "*.{js,cjs,mjs,ts}": ["eslint --fix", "prettier --write"],
+
+  "*.{json,css,scss,md}": ["prettier --write"],
+
+  "*.vue": ["eslint --fix", "prettier --write", "vue-tsc --noEmit --skipLibCheck"],
+};
