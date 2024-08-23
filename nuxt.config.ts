7798d3586c2d409a4bd5397e4962153b3bb812ff
BearingMe <gomes.bruno201@gmail.com>
2024-08-23 11:08:56 -0300

    chore: setup tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   srcDir: "src/",
-  modules: ["@nuxt/eslint"]
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+import { resolve } from "path";
+
+const srcDir = resolve(__dirname, "src");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {},
+  plugins: [],
+  content: [
+    `${srcDir}/components/**/*.{vue,js,ts}`,
+    `${srcDir}/layouts/**/*.vue`,
+    `${srcDir}/pages/**/*.vue`,
+    `${srcDir}/composables/**/*.{js,ts}`,
+    `${srcDir}/plugins/**/*.{js,ts}`,
+    `${srcDir}/utils/**/*.{js,ts}`,
+    `${srcDir}/App.{js,ts,vue}`,
+    `${srcDir}/app.{js,ts,vue}`,
+    `${srcDir}/Error.{js,ts,vue}`,
+    `${srcDir}/error.{js,ts,vue}`,
+    `${srcDir}/app.config.{js,ts}`,
+  ],
+};
