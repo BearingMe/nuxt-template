7837eef61089270933f78079785fd4ae971bec3b
BearingMe <gomes.bruno201@gmail.com>
2024-08-23 11:29:06 -0300

    chore: setup @tanstack/vue-query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import prettierConfig from "eslint-config-prettier";
+import tanstackConfig from "@tanstack/eslint-plugin-query";
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(prettierConfig);
+export default withNuxt(prettierConfig, tanstackConfig.configs["flat/recommended"]);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   srcDir: "src/",
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt", "@vueuse/nuxt"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@vueuse/nuxt",
+    "@hebilicious/vue-query-nuxt",
+  ],
 });
